beca4b3b07a8683f9189eaea7314f437caff9d37
Shinyu <k2023_3243@sttheresia-jkt.sch.id>
2025-08-20 12:36:06 +0700

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-  'FIREBASE_DB'     => 'https://padarincang-4fa0b-default-rtdb.asia-southeast1.firebasedatabase.app/',
-  'FIREBASE_SECRET' => 'nADz91eLLwA8ZeU4Hem3N2pXAzMQZEJ7lAP4s7BD'
-];
