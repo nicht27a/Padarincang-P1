ab32b32d518b8a7736b28c0e865c06982c1de23e
Nicholas <k2023_3243@sttheresia-jkt.sch.id>
2025-08-27 11:22:46 +0700

    add wave

--- public/main.php
+++ public/main.php
@@ -73,6 +73,12 @@
         <div class="scroll-indicator">
             <i class="fas fa-chevron-down"></i>
         </div>
+        <!-- Hero Wave -->
+        <div class="hero-wave">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <path fill="#ecfdf5" fill-opacity="1" d="M0,160L60,144C120,128,240,96,360,96C480,96,600,128,720,144C840,160,960,160,1080,144C1200,128,1320,96,1380,80L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+            </svg>
+        </div>
     </section>
 
     <!-- About Section -->
